c57e290b0be25b66a0f3fa5c3a41ccb8cd629159
Antonychelladurai <52097193+Antonychelladurai@users.noreply.github.com>
2020-02-23 21:19:11 +0530

    Spring Cloud Eureka mongoDB Actuter

---
+++ SpringCloudCID/src/main/java/com/anto/SpringCloudCID/SpringCloudCidApplication.java
@@ -0,0 +1,13 @@
+package com.anto.SpringCloudCID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudCidApplication.class, args);
+	}
+
+}
---
+++ SpringCloudCID/src/test/java/com/anto/SpringCloudCID/SpringCloudCidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anto.SpringCloudCID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
